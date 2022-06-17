9167cde937aaa9ec6b2a107eb7fc38d2897787ec
David Araújo <106712790+davidaraujosf@users.noreply.github.com>
2022-06-17 17:04:31 -0300

    Add files via upload

---
+++ calcula_idade.js
@@ -0,0 +1,19 @@
+function calcula_idade(anos){
+    return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
+};
+const pessoa1 = {
+    nome: "Maria",
+    idade: 30,
+};
+
+const pessoa2 = {
+    nome: "Paulo",
+    idade: 26,
+};
+const animal = {
+    nome: "Fiona",
+    idade: 5,
+    raca: "Pug"
+};
+
+console.log(calcula_idade.apply(animal, [20]))
